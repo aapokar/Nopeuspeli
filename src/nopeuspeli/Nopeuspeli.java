2555f39ba0c4b268b0a2d7af8e80cff675bba8c3
Aapo K <aapok@msn.com>
2019-08-20 20:32:14 +0300

    Aseteltu napit peliruutuun

--- src/nopeuspeli/Nopeuspeli.java
+++ src/nopeuspeli/Nopeuspeli.java
@@ -8,6 +8,8 @@ package nopeuspeli;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -42,12 +44,19 @@ public class Nopeuspeli extends Application {
         
 //        Pelinäkymän napit 0-3
         Button nappi0 = new Button("1");
+        nappi0.setPrefSize(30, 30);
         Button nappi1 = new Button("2");
+        nappi1.setPrefSize(30, 30);
         Button nappi2 = new Button("3");
+        nappi2.setPrefSize(30, 30);
         Button nappi3 = new Button("4");
+        nappi3.setPrefSize(30, 30);
         
 //        Luodaan nappien asettelu ja lisätään se pelinäkymään
         HBox nappienAsettelu = new HBox();
+        nappienAsettelu.setAlignment(Pos.CENTER);
+        nappienAsettelu.setPadding(new Insets(5,5,15,5));
+        nappienAsettelu.setSpacing(15);
         nappienAsettelu.getChildren().addAll(nappi0,nappi1,nappi2,nappi3);
         peliruudunAsettelu.setBottom(nappienAsettelu);
         
